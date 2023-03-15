c910fa6715dad2b0f4cf71203982ff450f193490
Guilherme Behs <guilherme.behs@sensedia.com>
2023-03-15 14:39:29 -0300

    chore: change migrations tu use pure sql

--- transactions/migrations/1678814984624-create-table-transaction.ts
+++ transactions/migrations/1678814984624-create-table-transaction.ts
@@ -7,55 +7,25 @@ import {
 
 export class createTableTransaction1678814984624 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(
-      new Table({
-        name: 'transaction',
-        columns: [
-          {
-            name: 'id',
-            type: 'bigint',
-            isPrimary: true,
-            isGenerated: true,
-            generationStrategy: 'increment',
-            isNullable: false,
-          },
-          {
-            name: 'account',
-            type: 'varchar(10)',
-            isNullable: false,
-          },
-          {
-            name: 'amount',
-            type: 'decimal(10, 2)',
-            isNullable: false,
-          },
-          {
-            name: 'status',
-            type: 'varchar(20)',
-            isNullable: false,
-          },
-          {
-            name: 'date',
-            type: 'timestamp',
-            isNullable: false,
-            default: 'now()',
-          },
-        ],
-      }),
+    await queryRunner.query(
+      `CREATE TABLE transaction (
+         id bigint NOT NULL AUTO_INCREMENT, 
+         account varchar(10) NOT NULL, 
+         amount decimal(10, 2) NOT NULL, 
+         status varchar(20) NOT NULL, 
+         date timestamp NOT NULL DEFAULT now(), 
+         PRIMARY KEY (id))`,
     );
-    await queryRunner.createForeignKey(
-      'transaction',
-      new TableForeignKey({
-        name: 'transaction_account_fk',
-        columnNames: ['account'],
-        referencedTableName: 'account',
-        referencedColumnNames: ['id'],
-      }),
+    await queryRunner.query(
+      `ALTER TABLE transaction
+        ADD CONSTRAINT transaction_account_fk FOREIGN KEY (account) REFERENCES account(id)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropForeignKey('transaction', 'transaction_account_fk');
-    await queryRunner.dropTable('transaction');
+    await queryRunner.query(
+      'ALTER TABLE transaction DROP FOREIGN KEY transaction_account_fk',
+    );
+    await queryRunner.query('DROP TABLE transaction');
   }
 }
