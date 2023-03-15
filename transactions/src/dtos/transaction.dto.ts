ddbb80121ef45211b2781f16c56ae69d7657824d
Guilherme Behs <guilherme.behs@sensedia.com>
2023-03-15 12:50:41 -0300

    docs: add swagger decorators to transaction dto

--- transactions/src/dtos/transaction.dto.ts
+++ transactions/src/dtos/transaction.dto.ts
@@ -1,10 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum } from 'class-validator';
 import { TransactionStatus } from 'src/enums/transaction-status.enum';
 
 export class TransactionDto {
+
+  @ApiProperty({
+    description: 'transaction id',
+  })
   id: number;
+
+  @ApiProperty({
+    description: 'account id',
+  })
   account: string;
+
+  @ApiProperty({
+    description: 'transaction amount',
+  })
   amount: number;
+
+  @ApiProperty({
+    description: 'transaction status',
+    enum: TransactionStatus
+  })
   status: TransactionStatus;
+
+  @ApiProperty({
+    description: 'transaction date',
+  })
   date: Date;
 
   constructor(
